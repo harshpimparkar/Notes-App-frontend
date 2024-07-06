7d5d8431be5b895b6a17bbb93f5de61add70c8b1
Harsh <harshypimparkar@gmail.com>
2024-07-06 11:54:21 +0530

    Navbar Test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow-md text-xl">
       <h2 className="text-xl font-bold text-black py-2 mr-2">
-        <div className="flex gap-2 items-center mr-5 font-extrabold">
+        <div className="flex gap-2 items-center mr-5 font-bold">
           <Link to="/">Notes.</Link>
         </div>
       </h2>
