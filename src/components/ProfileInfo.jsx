7bdcb1993085b9e64a2b57df7220d701432cfb51
Harsh <harshypimparkar@gmail.com>
2024-07-10 15:15:11 +0530

    Added profile

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -6,11 +6,11 @@ const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
     <>
       <div className="flex items-center justify-center gap-3 ml-5 drop-shadow-md border mr-5 px-5 py-1 text-white bg-primary hover:bg-blue-700 rounded-md">
-        {/* <div className="w-8 h-8 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+        <div className="w-8 h-8 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
           {getInitials(userInfo.fullname)}
-        </div> */}
+        </div>
         <div>
-          {/* <p className="text-sm font-bold">{userInfo.fullname}</p> */}
+          <p className="text-sm font-bold">{userInfo.fullname}</p>
           <button
             className="text-md text-white hover:underline"
             onClick={onLogout}
