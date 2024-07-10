d784d9d860d1deea16dc8d9a8c7a04051359e30b
Harsh <harshypimparkar@gmail.com>
2024-07-10 15:22:17 +0530

    profile removed

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,16 +1,16 @@
-import { getInitials } from "../utils/helper.js";
+// import { getInitials } from "../utils/helper.js";
 import { GrLogout } from "react-icons/gr";
 
 
-const ProfileInfo = ({ username , fullname, onLogout }) => {
+const ProfileInfo = ({ onLogout }) => {
   return (
     <>
       <div className="flex items-center justify-center gap-3 ml-5 drop-shadow-md border mr-5 px-5 py-1 text-white bg-primary hover:bg-blue-700 rounded-md">
-        <div className="w-8 h-8 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-          {getInitials(fullname)}
-        </div>
+        {/* <div className="w-8 h-8 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+          {getInitials(userInfo.fullname)}
+        </div> */}
         <div>
-          <p className="text-sm font-bold">{username}</p>
+          {/* <p className="text-sm font-bold">{userInfo.fullname}</p> */}
           <button
             className="text-md text-white hover:underline"
             onClick={onLogout}
